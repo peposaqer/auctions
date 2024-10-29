3c064ffa850957ac8f4bd05fc976a453d66685ac
peposaqer <mahmoudmetawea161@gmail.com>
2024-10-29 12:56:03 +0300

    newwwwwwww

--- controllers/AdminController/getendedsubcategory.js
+++ controllers/AdminController/getendedsubcategory.js
@@ -665,6 +665,7 @@ exports.adminActionOnWinner = async (req, res) => {
             // Remove winner from SubcategoryResult
             const subcategoryResult = await SubcategoryResult.findOne({ userId: user._id, subcategory: subcategory,status: 'winner' });
             console.log("winner",winner._id)
+console.log(subcategoryResult.results)
 
 
         if (subcategoryResult && subcategoryResult.results.includes(winner._id)) {
